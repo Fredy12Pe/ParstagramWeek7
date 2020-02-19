31ccfea39d720b0d28c50f9528e392c556bc3bb5
Fredy <fapedro@uci.edu>
2020-02-18 19:25:41 -0800

    Initial Commit

---
+++ ParstagramWeek7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParstagramWeek7
+//
+//  Created by Fredy Pedro on 2/18/20.
+//  Copyright © 2020 Fredy Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
